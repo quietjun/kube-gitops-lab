9bb9e52d805598c907806f54c0c665195d791739
hp <hp-stgray22@gmail.com>
2026-05-24 14:45:29 +0900

    great

--- springboot-app/hello/src/main/java/com/example/GreetingController.java
+++ springboot-app/hello/src/main/java/com/example/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s OMG!";
+	private static final String template = "Hello, %s Great!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello")
